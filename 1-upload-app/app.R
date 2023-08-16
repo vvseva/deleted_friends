0ef7ce45c9b1e93542b16048f1f4cd7d9e3af80d
vvseva <wirzet1@gmail.com>
2023-08-16 00:27:18 -0500

    questions rendered

--- 1-upload-app/app.R
+++ 1-upload-app/app.R
@@ -4,41 +4,57 @@ library(tidyverse)
 library(jsonlite)
 library(purrr)
 library(colorspace)
+library(shinyjs)
+library(digest)
 
 source("service.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  useShinyjs(),
 
     # Application title
-    titlePanel("Demo of reading a facebook file"),
+    titlePanel("Unfriending on Face"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-          "Instructions"
+          HTML("
+               <b> Instructions and opt-out: </b> <br>
+               In the following survey, we will ask questions about 
+               up to eight of your most recently removed Facebook 
+               connections, and up to eight of your current connections. 
+               If there are any individuals that you wish to avoid 
+               discussing, please uncheck their names here.
+               ")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-          fileInput("upload_deleted", "Upload a facebook file",
-                    accept = ".zip"),
-          # tableOutput("table_deleted")
-          htmlOutput("html_deleted")
+          fluidPage(
+          div(id = "upload_deleted_div",
+              fileInput("upload_deleted", "Upload a facebook file",
+                        accept = ".zip"),
+              htmlOutput("html_confirm_friends")
+              ),
+          div(id = "questions_1_div",
+              htmlOutput("html_deleted")
+              ),
+          div(id = "verification_div",
+              htmlOutput("html_verification")
+          )
         )
+    )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
 
-#   fb_df <- unzip("facebook-vvsuschevskiy (1).zip", "friends_and_followers/removed_friends.json") |> read_json()
-# 
-# 
-# fb_df |> 
-#   as_tibble() |> 
-#   unnest_wider(col = deleted_friends_v2) |> 
-#   mutate(timestamp = timestamp |> as.Date.POSIXct())
+  values <- reactiveValues(fb_df = NULL,
+                           fb_df_filtered = NULL)
+  
+  # values$fb_df <- tibble(name = NULL)
 
   observeEvent(input$upload_deleted, {
     
@@ -49,7 +65,7 @@ server <- function(input, output, session) {
       read_json()
     
     
-    fb_df <- fb_df |> 
+    values$fb_df <- fb_df |> 
       as_tibble() |> 
       unnest_wider(col = deleted_friends_v2) |> 
       mutate(timestamp = timestamp |> 
@@ -60,16 +76,86 @@ server <- function(input, output, session) {
                purple_colfunc() |> 
                lighten(amount = 0.75))
     
-    if (nrow(fb_df) == 0) {
-      # fb_df <- tibble(Error = "Sorry, there are no deleted friends")
-      fb_html <- HTML(h4("Sorry, there are no deleted friends"))
+    if (nrow(values$fb_df) == 0) {
+      output$html_deleted <- renderUI(
+        HTML(
+          h4("Sorry, there are no deleted friends")
+          )
+        )
     }
-
-    # output$table_deleted <- renderTable(fb_df)
-    output$html_deleted <- renderUI(fb_df |>
-                                      purrr::pmap(data.frame) |>
-                                      purrr::map(render_question))
+    
+    
+    output$html_confirm_friends <- renderUI(
+      column(
+        width = 8,
+        checkboxGroupInput(
+          inputId = "checkboxGroupInput_fb_cpnfirm",
+          label = "Selected deleted friends",
+          choices = values$fb_df$name,
+          selected  = values$fb_df$name
+        ),
+        actionButton(inputId = "actionButton_fb_confirm",
+                     label = "Confirm")
+      )
+    )
+    
+    
   })
+  
+  
+  
+  observeEvent(input$actionButton_fb_confirm, {
+    hide("upload_deleted_div")
+    
+    values$fb_df_filtered <- isolate(values$fb_df) |> 
+      filter(name %in% input$checkboxGroupInput_fb_cpnfirm)
+
+    if(nrow(values$fb_df_filtered) < 1){
+      ## TODO: message or something
+    } else {
+      output$html_deleted <- renderUI(
+        column(
+          width = 8,
+        fluidRow(
+        isolate(values$fb_df_filtered) |>
+          purrr::pmap(data.frame) |>
+          purrr::map(render_question),
+      ),
+      fluidRow(
+        div(
+          actionButton(inputId = "actionButton1_verify",
+                       label = "Next"),
+          style = "display:inline-block; float:right;"
+        ),
+      )
+      ))
+    }
+    
+  })
+  
+  observeEvent(input$actionButton1_verify, {
+    hide("questions_1_div")
+    
+    
+    output_df <- tibble(
+      user = session$token,
+      friend = 
+    )
+    
+    output$html_verification <- renderUI(
+      column(
+        width = 10,
+        renderTable(tibble(user = session$token,
+                           fri) |> 
+                      head(10)),
+        actionButton(inputId = "actionButton_sumbit",
+                   label = "Sumbit")
+      )
+    )
+    
+    
+  })
+  
   
   onStop(function() {
     print("session ended")
--- 1-upload-app/service.R
+++ 1-upload-app/service.R
@@ -1,5 +1,7 @@
 purple_colfunc <- colorRampPalette(c("#E4E0EE", "#836EAA"))
 
+## TODO: wrap block into a function
+
 render_question <- function(deleted_friend){
   
   f_name <- deleted_friend |> pull(name)
@@ -8,30 +10,128 @@ render_question <- function(deleted_friend){
   bg_color <- deleted_friend |> pull(bg_color)
   
   if (is_empty(deleted_friend)) {
-    column(
-      width = 8,
+    fluidRow(
+      # width = 12,
       div(class = "questions_div",
           p("Empty friend list")
           )
     )
   } else {
-    column(
-      width = 8,
-      div(class = "questions_div",
-          style = str_glue("width: 100%; background-color:{bg_color}; border-radius: 25px;"),
+    fluidRow(
+      # width = 6,
+      div(class = str_glue("questions_div_{id}"),
+          style = str_glue("box-sizing: border-box;
+                            margin: 25px;
+                           background-color:{bg_color}; 
+                           border-radius: 25px;
+                           background:linear-gradient(white, {bg_color});"),
           h4(str_glue("Who is {f_name}, that you deleted at {timestamp}")),
-          sliderInput(
-            # class = "slider_likert",
-            label = "How Much Is the Fish?",
-            min = 0, max = 100, value = 50,
-            inputId = str_glue("slider_likert_{id}", id = id)
+          h5("Political Ideology"),
+          radioButtons(
+            inputId = str_glue("radioButtons_political1_{id}"),
+            label = str_glue("Please indicate what you perceive {f_name} political ideology to be, to the best of your recollection."),
+            choices = c(
+            "Very Liberal", "Liberal", 
+            "Moderate", "Conservative", 
+            "Very Conservative", "Unsure")
             ),
-          textAreaInput(
-            # class = "areainput",
-            label = str_glue("Explain in few sentances why {f_name} ?"),
-            inputId = str_glue("areainput_{id}", id = id)
-          )
-      )
+          radioButtons(
+            inputId = str_glue("radioButtons_political2_{id}", id = id),
+            label = str_glue("How often, if ever, did you see {f_name} share things about political or social issues on Facebook?"),
+            choices = c(
+              "Frequently", "Rarely", 
+              "Never", "Unsure/don’t remember")
+          ),
+          h5("Religiosity"),
+          radioButtons(
+            inputId = str_glue("radioButtons_religion1_{id}", id = id),
+            label = str_glue("Please indicate what you perceive {f_name} religious affiliation to be, to the best of your recollection. "),
+            choices = c(
+              "Christian", "Muslim", 
+              "Jewish", "Hindu",
+              "Buddhist", "Atheist/Agnostic",
+              "Unsure",
+              "Other (please specify)")
+          ),
+          ## TODO: add conditional text for other
+          textInput(inputId = str_glue("textInput_religion_{id}", id = id),
+                    label = "Other (please specify)"),
+          radioButtons(
+            inputId = str_glue("radioButtons_religion2_{id}", id = id),
+            label = str_glue("Please indicate the extent to which you encounter(ed) religious content from {f_name},  to the best of your recollection."),
+            choices = c(
+              "Frequently", "Rarely", 
+              "Never")
+          ),
+          h5("Ethnic Status"),
+          radioButtons(
+            inputId = str_glue("radioButtons_ethinc1_{id}", id = id),
+            label = str_glue("Please indicate what you perceive {f_name}'s ethnicity to be, to the best of your recollection."),
+            choices = c(
+              "White or Caucasian", "Black or African American", 
+              "Hispanic or Latino", "Asian",
+              "Native American", "Native Hawaiian or Other Pacific Islander",
+              "Middle Eastern or Arab", "Multiracial/Mixed",
+              "Unsure",
+              "Other (please specify)")
+          ),
+          ## TODO: add conditional text for other
+          textInput(inputId = str_glue("textInput_ethinc_{id}", id = id),
+                    label = "Other (please specify)"),
+          h5("Gender Identity"),
+          radioButtons(
+            inputId = str_glue("radioButtons_gener_{id}", id = id),
+            label = str_glue("Please indicate what you perceive {f_name} gender to be, to the best of your recollection."),
+            choices = c(
+              "Male", "Female", 
+              "Non-binary/Third gender", "Unsure",
+              "Preffer to self-describe")
+          ),
+          ## TODO: add conditional text for other
+          textInput(inputId = str_glue("textInput_gender_{id}", id = id),
+                    label = "Preffer to self-describe"),
+          h5("Sexual Orientation"),
+          radioButtons(
+            inputId = str_glue("radioButtons_orientation_{id}", id = id),
+            label = str_glue("Please indicate what you perceive {f_name} sexual orientation to be, to the best of your recollection."),
+            choices = c(
+              "Heterosexual or Straight", "Unsure", 
+              "Preffer to self-describe")
+          ),
+          ## TODO: add conditional text for other
+          textInput(inputId = str_glue("textInput_orientation_{id}", id = id),
+                    label = "Preffer to self-describe"),
+          h5("Context"),
+          radioButtons(
+            inputId = str_glue("radioButtons_context_{id}", id = id),
+            label = str_glue("Please indicate the context that you have interacted with {f_name}, to the best of your recollection."),
+            choices = c(
+              "Residential", "Vocational", 
+              "Educational", "Religious",
+              "Recreational", "Military",
+              "None", "Other (please specify)")
+          ),
+          ## TODO: add conditional text for other
+          textInput(inputId = str_glue("textInput_context_{id}", id = id),
+                    label = "Other (please specify)"),
+          
+          h5("Social Relationships"),
+          radioButtons(
+            inputId = str_glue("radioButtons_relationship_{id}", id = id),
+            label = str_glue("Please describe the social relationship(s) that you have shared with {f_name}, to the best of your recollection (e.g., friend, coworker, etc.)."),
+            choices = c(
+              "Acquaintance", "Neighbor", 
+              "Friend", "Coworker or colleague",
+              "Teacher/student", "Family by blood",
+              "Family by marriage", "Romantic or sexual partner",
+              "None", "Other (please specify)")
+          ),
+          ## TODO: add conditional text for other
+          textInput(inputId = str_glue("textInput_relationship_{id}", id = id),
+                    label = "Other (please specify)"),
+      ),
+
+        
     )
   }
 }
