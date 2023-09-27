a117f2b9c42ea34dbae07dabb3954c86b5c86aaa
vvseva <wirzet1@gmail.com>
2023-09-26 22:47:37 -0500

    existing and removed friends demo

--- 1-upload-app/app.R
+++ 1-upload-app/app.R
@@ -65,6 +65,9 @@ ui <- fluidPage(
           div(id = "questions_1_div",
               htmlOutput("html_deleted")
               ),
+          div(id = "questions_2_div",
+              htmlOutput("html_existing")
+          ),
           div(id = "verification_div",
               htmlOutput("html_verification")
           )
@@ -76,7 +79,8 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
 
-  values <- reactiveValues(fb_df = NULL,
+  values <- reactiveValues(fb_df_removed = NULL,
+                           fb_df_existing = NULL,
                            fb_df_filtered = NULL,
                            logs = NULL)
   
@@ -84,59 +88,86 @@ server <- function(input, output, session) {
 
   observeEvent(input$upload_deleted, {
     
-    tryCatch({
+    fb_removed_df <- tryCatch({
       ### TODO: fix if not deleted friends
       
-      fb_df <- unzip(zipfile = input$upload_deleted$datapath,
+      unzip(zipfile = input$upload_deleted$datapath,
                      files  = "connections/friends/removed_friends.json",
                      exdir = tempdir()
       ) |> 
-        read_json()
+        read_json() |> 
+        as_tibble()
       
     }, error=function(e) {
-      cat(paste("in err handler2\n"),e)
-      fb_df <- tibble(deleted_friends_v2 = NULL)
+      # cat(paste("in err handler2\n"),e)
+      data.frame(deleted_friends_v2 = NULL, timestamp = NULL)
     }, warning=function(w) {
-      cat(paste("in warn handler2\n"),w)
-      fb_df <- tibble(deleted_friends_v2 = NULL)
+      # cat(paste("in warn handler2\n"),w)
+      data.frame(deleted_friends_v2 = NULL, timestamp = NULL)
     })
-    
 
-    
-    
-    values$fb_df <- fb_df |> 
-      as_tibble() |> 
-      unnest_wider(col = deleted_friends_v2) |> 
-      mutate(timestamp = timestamp |> 
-               as.Date.POSIXct()) |> 
-      head(10) |> 
-      mutate(id = row_number()) |> 
-      mutate(bg_color = n() |> 
-               purple_colfunc() |> 
+    if (nrow(fb_removed_df) < 1)  {
+      output$html_confirm_friends <- renderUI(
+          h4("Sorry, you have not deleted any friends in the past 3 years, and therefore you are not qualify for the next part of the study")
+      )
+    } else {
+      
+  
+      
+      fb_removed_df <- fb_removed_df |>
+        unnest_wider(col = deleted_friends_v2) |>
+        mutate(timestamp = timestamp |>
+                 as.Date.POSIXct())
+      
+    values$fb_df_removed <- fb_removed_df |>
+      head(10) |>
+      mutate(id = row_number()) |>
+      mutate(bg_color = n() |>
+               purple_colfunc() |>
                lighten(amount = 0.75))
     
-    if (nrow(values$fb_df) == 0) {
-      output$html_deleted <- renderUI(
-        HTML(
-          h4("Sorry, you have not deleted any friends in the past 3 years, and therefore you are not qualify for the next part of the study")
-          )
-        )
-    }
+    fb_your_df <- tryCatch({
+      
+      unzip(zipfile = input$upload_deleted$datapath,
+            files  = "connections/friends/your_friends.json",
+            exdir = tempdir()
+      ) |> read_json() 
+      
+    }, error=function(e) {
+      data.frame(friends_v2 = NULL, timestamp = NULL)
+    }, warning=function(w) {
+      data.frame(friends_v2 = NULL, timestamp = NULL)
+    })
     
-    
+    values$fb_df_existing <- fb_your_df |>
+      as_tibble() |>
+      unnest_wider(col = friends_v2) |>
+      mutate(timestamp = timestamp |>
+               as.Date.POSIXct()) |>
+      filter(!name %in% fb_removed_df$name) |> 
+      sample_n(size = nrow(fb_removed_df)) |> ## TODO: replace with 5?
+      mutate(id = row_number()) |>
+      mutate(bg_color = n() |>
+               purple_colfunc() |>
+               lighten(amount = 0.75))
+
+
     output$html_confirm_friends <- renderUI(
       column(
         width = 8,
         checkboxGroupInput(
           inputId = "checkboxGroupInput_fb_cpnfirm",
           label = "Selected deleted friends",
-          choices = values$fb_df$name,
-          selected  = values$fb_df$name
+          choices = c(values$fb_df_existing$name, values$fb_df_removed$name),
+          selected  = c(values$fb_df_existing$name, values$fb_df_removed$name)
         ),
         actionButton(inputId = "actionButton_fb_confirm",
                      label = "Confirm")
       )
     )
+  }
+    
+  
     
     
   })
@@ -146,23 +177,27 @@ server <- function(input, output, session) {
   observeEvent(input$actionButton_fb_confirm, {
     hide("upload_deleted_div")
     
-    values$fb_df_filtered <- isolate(values$fb_df) |> 
+    values$fb_df_removed_filtered <- isolate(values$fb_df_removed) |> 
       filter(name %in% input$checkboxGroupInput_fb_cpnfirm)
 
-    if(nrow(values$fb_df_filtered) < 1){
+    if(nrow(values$fb_df_removed_filtered) < 1){
       ## TODO: message or something
+      output$html_deleted <- renderUI(
+        H4("No removed friends left :(")
+      )
+
     } else {
       output$html_deleted <- renderUI(
         column(
           width = 8,
         fluidRow(
-        isolate(values$fb_df_filtered) |>
+        isolate(values$fb_df_removed_filtered) |>
           purrr::pmap(data.frame) |>
           purrr::map(render_question),
       ),
       fluidRow(
         div(
-          actionButton(inputId = "actionButton1_verify",
+          actionButton(inputId = "actionButton1_existing",
                        label = "Next"),
           style = "display:inline-block; float:right;"
         ),
@@ -172,8 +207,42 @@ server <- function(input, output, session) {
     
   })
   
-  observeEvent(input$actionButton1_verify, {
+  observeEvent(input$actionButton1_existing, {
     hide("questions_1_div")
+    
+    
+    values$fb_df_existing_filtered <- isolate(values$fb_df_existing) |> 
+      filter(name %in% input$checkboxGroupInput_fb_cpnfirm)
+    
+    if(nrow(values$fb_df_existing_filtered) < 1){
+      ## TODO: message or something
+      output$html_deleted <- renderUI(
+        H4("No existing friends left :(")
+      )
+      
+    } else {
+      output$html_existing <- renderUI(
+        column(
+          width = 8,
+          fluidRow(
+            isolate(values$fb_df_removed_filtered) |>
+              purrr::pmap(data.frame) |>
+              purrr::map(render_question_2),
+          ),
+          fluidRow(
+            div(
+              actionButton(inputId = "actionButton1_verify",
+                           label = "Next"),
+              style = "display:inline-block; float:right;"
+            ),
+          )
+        ))
+    }
+    
+  })
+  
+  observeEvent(input$actionButton1_verify, {
+    hide("questions_2_div")
     
     ## TODO: replace with loop map?
     
